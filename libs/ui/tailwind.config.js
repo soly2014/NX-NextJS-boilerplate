c6332f702a45c695ecc7d79581c0b8cc4e609ec2
solly-lynk <123766905+solly-awaed@users.noreply.github.com>
2024-06-11 01:27:56 +0300

    finish installing storybook

---
+++ libs/ui/tailwind.config.js
@@ -0,0 +1,10 @@
+const { join } = require('path');
+
+module.exports = {
+  presets: [require('../../tailwind.config.js')],
+  content: [join(__dirname, './src/**/*.{js,jsx,ts,tsx}')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

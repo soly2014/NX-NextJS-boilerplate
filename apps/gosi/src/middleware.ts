d48bed2e3686f62317f88ced106c8a30999ef732
soly2014 <solyooo398523@gmail.com>
2024-10-07 16:27:03 +0800

    change default locale

--- apps/gosi/src/middleware.ts
+++ apps/gosi/src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'ar'],
+  locales: ['ar', 'en'],
 
   // Used when no locale matches
   defaultLocale: 'ar',

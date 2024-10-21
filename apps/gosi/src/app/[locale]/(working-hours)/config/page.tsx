f01b7e49b7828c33a48245217fb90ae2a801055a
soly2014 <solyooo398523@gmail.com>
2024-10-21 21:05:23 +0800

    fix splash message issue

--- apps/gosi/src/app/[locale]/(working-hours)/config/page.tsx
+++ apps/gosi/src/app/[locale]/(working-hours)/config/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 import React from 'react';
 import { ConfigPage } from '@pages';
-import { OutOfWorkingHours } from '@ui';
+import { FullPageLoading, OutOfWorkingHours } from '@ui';
 import { useWorkingHoursAllQuery } from '@gosi/queries/useWorkingHours';
 import { useSearchParams } from 'next/navigation';
 
 const Page = () => {
-  const { data, isSuccess } = useWorkingHoursAllQuery(); // Use the new hook
+  const { data, isSuccess, isLoading } = useWorkingHoursAllQuery(); // Use the new hook
   const searchParams = useSearchParams();
 
   const serviceType = searchParams.get('serviceType');
+
+  // Ensure that we render nothing until the data is loaded
+  if (isLoading) {
+    return <FullPageLoading />; // or a loading indicator if preferred
+  }
 
   if (isSuccess && serviceType && !data[serviceType]) {
     return <OutOfWorkingHours />;
--- libs/ui/src/components/full-page-loading/index.tsx
+++ libs/ui/src/components/full-page-loading/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const FullPageLoading = () => {
   return (
     <div className="flex min-h-[calc(100dvh-10.2rem)] flex-col items-center justify-center px-4">
-      <div className="h-24 w-24 animate-spin rounded-full border-b-4 border-t-4 border-primary"></div>
+      <div className="h-12 w-12 animate-spin rounded-full border-b-4 border-t-4 border-primary"></div>
     </div>
   );
 };
